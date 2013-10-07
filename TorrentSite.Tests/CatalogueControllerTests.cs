032ec865d071d6a799dce9f22c5d13f08010fc08
Evlogi Hristov <evlogihristov@yahoo.com>
2013-10-08 01:17:37 +0300

    Added CatalogueTests class

---
+++ TorrentSite.Tests/CatalogueControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TorrentSite.Controllers;
+using System.Web.Mvc;
+using System.Collections;
+using TorrentSite.Models;
+using System.Collections.Generic;
+
+namespace TorrentSite.Tests
+{
+    [TestClass]
+    public class CatalogueControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            var controller = new CataloguesController();
+            var viewResult = controller.Index() as ViewResult;
+            Assert.IsNotNull(viewResult, "Index action does not return View");
+
+            var model = viewResult.Model as IEnumerable<Catalogue>
+
+
+        }
+    }
+}
